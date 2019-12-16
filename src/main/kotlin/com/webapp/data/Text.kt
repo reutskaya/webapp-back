22535822e9a6ead81c00fc9642d95044bb00e7af
reutskaya <32382007+reutskaya@users.noreply.github.com>
2019-12-16 22:47:40 +0300

    Application controller

--- src/main/kotlin/com/webapp/data/Text.kt
+++ src/main/kotlin/com/webapp/data/Text.kt
@@ -1,5 +1,6 @@
 package com.webapp.data
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Field
 
 data class Text(@Id val id: Int? = null, val text: String)
